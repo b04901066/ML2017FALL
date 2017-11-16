045cb78cdb4da68f08a44ce27bc7f68ae3f0ae7c
洪國喨 <31993733+b04901066@users.noreply.github.com>
2017-11-16 17:23:40 +0800

    Update hw3_test.sh

--- hw3/hw3_test.sh
+++ hw3/hw3_test.sh
@@ -3,5 +3,8 @@ wget 'https://www.dropbox.com/s/e9n9ipmxn4quc3p/best2.h5?dl=1' -O './best2.h5'
 wget 'https://www.dropbox.com/s/t4tkc408ykrcxp5/hw3_65.h5?dl=1' -O './hw3_65.h5'
 wget 'https://www.dropbox.com/s/9w3i0s7ayuwngvf/hw3_Liu.h5?dl=1' -O './hw3_Liu.h5'
 wget 'https://www.dropbox.com/s/or4j7gagrvoajcz/u.h5?dl=1' -O './u.h5'
-python3 ./hw3_test.py $1 $2 ./best2.h5 ./hw3_65.h5 ./hw3_Liu.h5 ./u.h5
+wget 'https://www.dropbox.com/s/zhyhxsn9aeh62wi/u1.h5?dl=1' -O './u1.h5'
+wget 'https://www.dropbox.com/s/aj88jpxpibytt6k/u2.h5?dl=1' -O './u2.h5'
+wget 'https://www.dropbox.com/s/kw7bmajnne8s6iw/u3.h5?dl=1' -O './u3.h5'
+python3 ./hw3_test.py $1 $2 ./best2.h5 ./hw3_65.h5 ./hw3_Liu.h5 ./u.h5 ./u1.h5 ./u2.h5 ./u3.h5
 exit 0
